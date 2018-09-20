ba5ed09d5358f121b31dbc923229e8d863c17fd5
Anders <Andershaesling@gmail.com>
2018-09-20 11:49:28 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,70 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+        // Opgave 1.1
+        // Write a program a "Welcome to Java"
+        // Leaning Java now
+        // Programming is FUN
+
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java now");
+        System.out.println("Programming is fun");
+
+        System.out.println();
+
+        //Opgave 1.2
+        // Write a program that displays " I Love Java" five times.
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+
+        // Opgave 1.3
+        //Write a program  that displays the following pattern
+
+        System.out.println("a   a2   a3   a4");
+        System.out.println("1   1    1    1");
+        System.out.println("2   4    6    8");
+        System.out.println("3   9    27   81");
+        System.out.println("4   16   64   256");
+        System.out.println();
+
+        //Opgave 1.5
+        //Write a program that the result of
+
+        double a = (7.5*6.5-4.5*3/47.5-5.5);
+        System.out.println();
+        System.out.println(a);
+
+        System.out.println();
+
+        // Opgave 1.6
+
+        System.out.println(1+2+3+4+6+7+8+9+10);
+        System.out.println();
+
+        //Opgave 1.7
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
